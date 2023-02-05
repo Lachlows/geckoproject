ef74a71ed491d054e2cf4aae6f5f4835d2688f8c
Gommure <laura.deenis@gmail.com>
2023-02-05 11:17:21 +0100

    startScreen & load le jeu avec le bouton play

--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SceneManager : MonoBehaviour
+public class Scenes : MonoBehaviour
 {
+    public Button playButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class SceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playButton.Select();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("mainScene");
     }
 }
